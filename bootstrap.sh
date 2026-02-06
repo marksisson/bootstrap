e2b01d9c1ce79126961de99a29c657d8544369ca
Mark Sisson <5761292+marksisson@users.noreply.github.com>
2026-02-06 17:48:43 -0600

    Change nix shell command to use bash

--- bootstrap.sh
+++ bootstrap.sh
@@ -34,7 +34,7 @@ nix registry add nixpkgs $NIXPKGS
 # install packages needed by remainder of script
 if [ -z "${SCRIPT_IN_NIX_SHELL:-}" ]; then
   export SCRIPT_IN_NIX_SHELL=1
-  exec nix shell nixpkgs#git nixpkgs#gnupg --command "$0" "$@"
+  exec nix shell nixpkgs#bash nixpkgs#git nixpkgs#gnupg --command bash "$0" "$@"
 fi
 
 # install gnupg configuration
